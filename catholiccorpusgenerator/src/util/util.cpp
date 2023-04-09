46aa7fd754cc6d7359fa4ac21bfc09a6eabb3ee6
ArnaudGidouinCatho <gatoongidouin@gmail.com>
2023-04-09 21:37:02 +0200

    Remove beginOfChapter with a letter

--- catholiccorpusgenerator/src/util/util.cpp
+++ catholiccorpusgenerator/src/util/util.cpp
@@ -1,4 +1,5 @@
 #include "util.hpp"
+#include <iostream>
 #include <vector>
 
 namespace
@@ -22,28 +23,57 @@ bool isOnlySpace(const std::string& pLine)
 
 std::string removeBeginOfChapterNumber(const std::string& pLine)
 {
+  if (pLine == " a- Aristote. ")
+    std::cout << "Toto " <<std::endl;
   static const std::vector<std::string> stringsToEscape = {"–","","●","→","","•",""};
+  std::size_t numberOfGoodCharacters = 0;
+  std::size_t beginOfLine = 0;
   std::size_t i = 0;
   while (i < pLine.size())
   {
-    if (pLine[i] == ' ' || _isDigit(pLine[i]) || pLine[i] == '-')
+    bool isGoodCharacter = true;
+    if (numberOfGoodCharacters == 0)
     {
-      ++i;
-      continue;
-    }
-
-    bool shouldBreak = true;
-    for (const auto& currStr : stringsToEscape)
-    {
-      if (pLine.size()-i >= currStr.size() && pLine.compare(i, currStr.size(), currStr) == 0)
+      if (pLine[i] == ' ' || _isDigit(pLine[i]) || pLine[i] == '-')
       {
-        i = i + currStr.size();
-        shouldBreak = false;
+        ++i;
+        beginOfLine = i;
+        continue;
+      }
+      for (const auto& currStr : stringsToEscape)
+      {
+        if (pLine.size()-i >= currStr.size() && pLine.compare(i, currStr.size(), currStr) == 0)
+        {
+          i = i + currStr.size();
+          beginOfLine = i;
+          isGoodCharacter = false;
+          break;
+        }
+      }
+    }
+    else
+    {
+      if (pLine[i] == ' ')
+      {
+        ++i;
+        continue;
+      }
+      if (pLine[i] == '-')
+      {
+        ++i;
+        beginOfLine = i;
         break;
       }
     }
-    if (shouldBreak)
-      break;
+
+    if (isGoodCharacter)
+    {
+      if (numberOfGoodCharacters > 0)
+        break;
+      ++numberOfGoodCharacters;
+      ++i;
+    }
   }
-  return pLine.substr(i, pLine.size()-i);
+
+  return pLine.substr(beginOfLine, pLine.size() - beginOfLine);
 }
