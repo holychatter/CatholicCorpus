3b47da55c1d94b74aa2649211628dcb7d33876b8
ArnaudGidouinCatho <gatoongidouin@gmail.com>
2023-05-28 20:32:10 +0200

    Add Le Livre de la vie to Carmel

--- catholiccorpusgenerator/src/main.cpp
+++ catholiccorpusgenerator/src/main.cpp
@@ -25,6 +25,13 @@ int main(int argc, char *argv[])
 
   {
     auto saintethereseavila_parser = std::make_unique<SainteThereseAvila_Parser>(
+          "Carmel/SainteThereseAvila/le_livre_de_la_vie-2.txt",
+          "Prologue ");
+    saintethereseavila_parser->run();
+  }
+
+  {
+    auto saintethereseavila_parser = std::make_unique<SainteThereseAvila_Parser>(
           "Carmel/SainteThereseAvila/le_chemin_de_perfection.txt",
           "Prologue ");
     saintethereseavila_parser->run();
